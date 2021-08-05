462c5d1e0f34882a5f151b0b4a6f329594ab706c
yiruhe <1170514059@qq.com>
2021-08-05 14:25:42 +0800

    Update DefaultNSocketClientConnector.java

--- client/DefaultNSocketClientConnector.java
+++ client/DefaultNSocketClientConnector.java
@@ -1,12 +1,10 @@
-package netty_demo.client;
+package com.netty.client;
 
+import com.netty.base.ChannelHandlerHolder;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import netty_demo.base.ChannelHandlerHolder;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class DefaultNSocketClientConnector extends NettyClientConnector{
@@ -15,12 +13,12 @@ public class DefaultNSocketClientConnector extends NettyClientConnector{
 
     private int tryCount=0;
 
-    private ChannelHandlerHolder channelHandlers = null;
+    private ChannelHandlerHolder channelHandlers;
 
 
     public ChannelFuture connect(String host, int port, ChannelHandlerHolder channelHandlers) throws InterruptedException {
 
-        this.channelHandlers = channelHandlers;
+            this.channelHandlers = channelHandlers;
 
         return  this.connect(host,port);
     }
@@ -46,13 +44,9 @@ public class DefaultNSocketClientConnector extends NettyClientConnector{
                 }else{
                     return new ChannelHandler[]{
                             this
-                            //*,
-                            //每隔30s的时间触发一次userEventTriggered的方法，并且指定IdleState的状态位是WRITER_IDLE
-                            // new IdleStateHandler(2, 0, 0, TimeUnit.SECONDS),
-                            //实现userEventTriggered方法，并在state是WRITER_IDLE的时候发送一个心跳包到sever端，告诉server端我还活着
-                            //  new ConnectorIdleStateTrigger()*//*
                     };
                 }
+
             }
 
         };
@@ -68,10 +62,12 @@ public class DefaultNSocketClientConnector extends NettyClientConnector{
                 }
             });
 
-            future = boot.connect(host, port);
+
+            future = doConnection(host,port);
+           // future = boot.connect(host, port);
         }
 
-        future.addListener(new ChannelFutureListener(){
+        /*future.addListener(new ChannelFutureListener(){
 
             @Override
             public void operationComplete(ChannelFuture future) throws Exception {
@@ -86,6 +82,39 @@ public class DefaultNSocketClientConnector extends NettyClientConnector{
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
+                        }, 3000, TimeUnit.MILLISECONDS);    // 设置 3s 进行一次 “重连”
+                    }else{
+                        System.out.println("连接失败，关闭服务");
+                        boot.config().group().shutdownGracefully();
+                    }
+
+                } else {
+                    System.out.println("Connect Netty Server successfully!");
+                }
+            }
+        });*/
+
+
+        ChannelFuture sync = future.sync();
+
+        return sync;
+    }
+
+    private ChannelFuture doConnection(String host, int port) {
+
+        final Bootstrap boot = bootstrap();
+        ChannelFuture future = boot.connect(host, port);
+        future.addListener(new ChannelFutureListener(){
+
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (!future.isSuccess()) {  // 没有连接成功，进行 “重连”
+                    if(tryCount < 3){
+                        tryCount++;
+                        future.channel().eventLoop().schedule(() -> {
+                            System.out.println("Connection Netty Server failed, trying to reconnect..."+tryCount);
+
+                            doConnection(host, port);
 
                         }, 3000, TimeUnit.MILLISECONDS);    // 设置 3s 进行一次 “重连”
                     }else{
@@ -99,10 +128,9 @@ public class DefaultNSocketClientConnector extends NettyClientConnector{
             }
         });
 
-        ChannelFuture sync = future.sync();
-
-        return sync;
+        return future;
     }
+
 
     public DefaultNSocketClientConnector() {
         init();
@@ -120,7 +148,6 @@ public class DefaultNSocketClientConnector extends NettyClientConnector{
         bootstrap().option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.TCP_NODELAY, true)
                 .option(ChannelOption.ALLOW_HALF_CLOSURE, false);
-
     }
 
     @Override
